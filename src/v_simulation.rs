f59027f6ce5b98460f451b95b33ed3647216d70b
Apoxtrophe <cyggardner@gmail.com>
2023-12-27 17:52:31 -0700

    fixed some propagation issues

--- src/v_simulation.rs
+++ src/v_simulation.rs
@@ -20,10 +20,8 @@ pub fn logic_operation_system(
                 TypeVoxel::Out => {
                     let is_on = process_out_logic(position_voxel.0, &voxel_query);
                     changes.push((entity, is_on));
-                    if is_on {
-                        println!("Starting propagation from: {:?}", position_voxel.0); // Debug print
-                        dfs_propagate(position_voxel.0, &voxel_query, &mut visited, &mut changes);
-                    }
+                    println!("Starting propagation from: {:?}", position_voxel.0); // Debug print
+                    dfs_propagate(position_voxel.0, &voxel_query, &mut visited, is_on, &mut changes);
                 },
                 // Add other voxel types here if needed
                 _ => (),
@@ -37,6 +35,7 @@ fn dfs_propagate(
     current_position: IVec3,
     voxel_query: &Query<(Entity, &PositionVoxel, &TypeVoxel, &mut StateVoxel)>,
     visited: &mut HashSet<IVec3>,
+    new_state: bool,
     changes: &mut Vec<(Entity, bool)>
 ) {
     // Check if the current voxel is already visited
@@ -53,13 +52,13 @@ fn dfs_propagate(
         // Find adjacent wire voxels and propagate the state
         for (entity, pos_voxel, type_voxel, _) in voxel_query.iter() {
             if *pos_voxel == PositionVoxel(*adj_pos) && matches!(type_voxel, TypeVoxel::Wire) {
-                println!("Propagating to: {:?}", adj_pos); // Debug print
+                println!("Propagating to: {:?} with state: {}", adj_pos, new_state); // Debug print
 
                 // Add the adjacent wire voxel to the changes vector
-                changes.push((entity, true));
+                changes.push((entity, new_state));
 
                 // Recursively call dfs_propagate to continue the propagation
-                dfs_propagate(*adj_pos, voxel_query, visited, changes);
+                dfs_propagate(*adj_pos, voxel_query, visited, new_state, changes);
             }
         }
     }
@@ -74,7 +73,7 @@ fn process_out_logic(
     get_adjacent_positions(position).iter()
         .any(|adj_pos| voxel_query.iter()
             .any(|(_, pos_voxel, type_voxel, state_voxel)| 
-                *adj_pos == pos_voxel.0 && !matches!(type_voxel, TypeVoxel::Tile | TypeVoxel::Out) && state_voxel.0))
+                *adj_pos == pos_voxel.0 && !matches!(type_voxel, TypeVoxel::Tile |TypeVoxel::Wire| TypeVoxel::Out) && state_voxel.0))
 }
 
 fn apply_changes(
